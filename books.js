74b9292a1658d9833939fee2c438b579624e6d80
Mwininia-Saanyeh <ramoakidas@gmail.com>
2022-09-05 14:57:21 +0000

    first commit

---
+++ books.js
@@ -0,0 +1,6 @@
+const books =[
+   {title:"rich dad poor dad",authur:"Robert Kiyosarki",publication:"23/3/2000",type:"non-fiction", ISBN:"isbnr234"}, 
+   {title:"no sweetness here",authur:"Ama Atta Aidoo",publication:"23/3/2000",type:"non-fiction", ISBN:"isbnr234"}, 
+   { title:"dilema of a ghost",authur:"Ama Atta Aidoo",publication:"1/12/1987",type:"fiction", ISBN:"isbnr234"}
+]
+module.exports=books
